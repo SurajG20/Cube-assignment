15e158faee40a036e4f8a6e2859522c2aa2d50f0
Suraj Goswami <121960253+SurajG20@users.noreply.github.com>
2024-08-11 03:25:56 +0530

    chore: Add initial project files and configurations

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <section className='body-wrapper'>
+        <h1>React Assignment</h1>
+        <div className='body-container'>
+          <div className='list'>
+            <div className='list-item'>
+              <p>Customer 1</p>
+              <div className='title-content'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque animi odit soluta recusandae,
+              </div>
+            </div>
+            <div className='list-item'>
+              <p>Customer 1</p>
+              <div className='title-content'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque animi odit soluta recusandae,
+              </div>
+            </div>
+            <div className='list-item'>
+              <p>Customer 1</p>
+              <div className='title-content'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque animi odit soluta recusandae,
+              </div>
+            </div>
+            <div className='list-item'>
+              <p>Customer 1</p>
+              <div className='title-content'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque animi odit soluta recusandae,
+              </div>
+            </div>
+            <div className='list-item'>
+              <p>Customer 1</p>
+              <div className='title-content'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque animi odit soluta recusandae,
+              </div>
+            </div>
+            <div className='list-item'>
+              <p>Customer 1</p>
+              <div className='title-content'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque animi odit soluta recusandae,
+              </div>
+            </div>
+            <div className='list-item'>
+              <p>Customer 1</p>
+              <div className='title-content'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque animi odit soluta recusandae,
+              </div>
+            </div>
+          </div>
+          <div className='details'>
+            <div className='content-container'>
+              <h2>Customer 1</h2>
+              <div className='title'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat fuga optio blanditiis! Quam, quisquam
+                reiciendis natus at officiis mollitia dolores sed ut error omnis ad atque reprehenderit iusto suscipit
+                placeat?
+              </div>
+              <div className='address'>
+                <span className='address-title'>Address:</span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel ex quam excepturi.
+              </div>
+            </div>
+            <div className='image-container'>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+              <div>6</div>
+              <div>7</div>
+              <div>8</div>
+              <div>9</div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
